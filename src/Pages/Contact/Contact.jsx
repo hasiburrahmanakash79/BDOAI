8c68f1ea51bc1f86b9203394b6f1e8acb6c6e370
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-04-20 13:44:04 +0600

    make contact form

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,29 @@
+import { Button, Input, Textarea } from "@material-tailwind/react";
+import { FaCommentDots } from "react-icons/fa6";
 
 const Contact = () => {
-    return (
-        <div>
-            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima dolores commodi ipsum accusamus error quis harum? Reprehenderit eos error optio ex culpa distinctio, magni voluptas magnam ullam deserunt! Placeat quos magni officia voluptatibus, nisi sint neque ipsa optio est veritatis repellendus quod officiis, quia accusamus. Quia vel aliquid omnis alias.</h1>
+  return (
+    <div>
+      <h1 className="text-7xl font-bold text-blue-700 text-center my-7">
+        Contact us
+      </h1>
+      <div className="space-y-8 w-1/3 mx-auto">
+        <p>
+          Use the form below to get in touch with our team and ask any questions
+        </p>
+        <div className="divider text-5xl">
+          <FaCommentDots />
         </div>
-    );
+        <Input color="blue" label="Your Name" />
+        <Input color="blue" label="Your Email" />
+        <Input color="blue" label="Country" />
+        <Textarea color="blue" label="Your Message" />
+        <Button color="blue" variant="gradient" className="rounded-full w-full">
+        Submit
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
